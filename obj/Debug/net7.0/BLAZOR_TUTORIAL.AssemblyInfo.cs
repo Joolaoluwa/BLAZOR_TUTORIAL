e47804d08db000692ec0e653949fec5b2870a2eb
olusola joshua <joolaoluwa2002@gmail.com>
2024-02-09 12:07:48 +0100

    New changes

--- obj/Debug/net7.0/BLAZOR_TUTORIAL.AssemblyInfo.cs
+++ obj/Debug/net7.0/BLAZOR_TUTORIAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLAZOR_TUTORIAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feed00eb7b714a4fb2f3ae9b9165b48784c4831")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLAZOR_TUTORIAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLAZOR_TUTORIAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
